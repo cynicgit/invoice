2c06921cdf5bea0679cb73f095bbe1ccf57096cd
cynic <huangyou70@gmail.com>
2019-02-14 23:45:10 +0800

    sql添加

---
+++ 1.sql
@@ -0,0 +1,58 @@
+/*
+SQLyog Ultimate v11.24 (32 bit)
+MySQL - 5.7.15 : Database - zhongyi
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`zhongyi` /*!40100 DEFAULT CHARACTER SET utf8mb4 */;
+
+USE `zhongyi`;
+
+/*Table structure for table `t_department` */
+
+DROP TABLE IF EXISTS `t_department`;
+
+CREATE TABLE `t_department` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(100) DEFAULT NULL,
+  `pid` int(10) unsigned NOT NULL,
+  `gmt_create` timestamp NULL DEFAULT NULL,
+  `gmt_modified` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4;
+
+/*Data for the table `t_department` */
+
+insert  into `t_department`(`id`,`name`,`pid`,`gmt_create`,`gmt_modified`) values (1,'环卫2',0,NULL,NULL),(2,'环境',3,NULL,NULL),(3,'测速',0,NULL,NULL);
+
+/*Table structure for table `t_user` */
+
+DROP TABLE IF EXISTS `t_user`;
+
+CREATE TABLE `t_user` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(100) DEFAULT NULL,
+  `dep_id` int(11) NOT NULL,
+  `type` int(1) DEFAULT '0',
+  `gmt_create` datetime DEFAULT NULL,
+  `gmt_modified` datetime DEFAULT NULL,
+  `password` varchar(100) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4;
+
+/*Data for the table `t_user` */
+
+insert  into `t_user`(`id`,`name`,`dep_id`,`type`,`gmt_create`,`gmt_modified`,`password`) values (1,'111',2,0,NULL,NULL,NULL),(2,'1111',1,1,NULL,NULL,'b0baee9d279d34fa1dfd71aadb908c3f');
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
